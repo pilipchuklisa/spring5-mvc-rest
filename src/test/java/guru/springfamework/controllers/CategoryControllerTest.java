93cea7e9b603a876581a99b32ff168ec6cf47ff1
Lisa <pilipchchlisa@gmail.com>
2025-01-30 15:54:27 +0300

    added Category Controller

---
+++ src/test/java/guru/springfamework/controllers/CategoryControllerTest.java
@@ -0,0 +1,73 @@
+package guru.springfamework.controllers;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.services.CategoryService;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryControllerTest {
+
+    @InjectMocks
+    CategoryController categoryController;
+
+    @Mock
+    CategoryService categoryService;
+
+    MockMvc mockMvc;
+
+    public static final Long ID = 1L;
+    public static final String NAME = "name";
+    public static final String API_URL = "/api/v1/categories/";
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(categoryController).build();
+    }
+
+    @Test
+    public void getAllCategories() throws Exception {
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setId(ID);
+        categoryDTO.setName(NAME);
+
+        List<CategoryDTO> categoryDTOS = new ArrayList<>();
+        categoryDTOS.add(categoryDTO);
+
+        Mockito.when(categoryService.getAllCategories()).thenReturn(categoryDTOS);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(API_URL)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.categories", Matchers.hasSize(1)));
+
+    }
+
+    @Test
+    public void getCategoryByName() throws Exception {
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setId(ID);
+        categoryDTO.setName(NAME);
+
+        Mockito.when(categoryService.getCategoryByName(NAME)).thenReturn(categoryDTO);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(API_URL + NAME)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.equalTo(NAME)));
+    }
+}
